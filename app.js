e157096eb9c7efc119b3c81a007bd62b6b27e1ea
aweizman <adamweizman@gmail.com>
2021-06-16 13:31:14 -0400

    should work on heroku now

--- app.js
+++ app.js
@@ -7,4 +7,11 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+client.on('message', message => {
+	console.log(message.author + ': ' + message.content);
+	if (message.content === '!ping') {
+		message.channel.send('Pong');
+	}
+});
+
 client.login(process.env.TOKEN);
