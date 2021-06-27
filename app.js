b3fac42ab204475eac795f98c1db39dc015c77d8
aweizman <adamweizman@gmail.com>
2021-06-27 16:45:09 -0400

    added variable delimiter to be able to be changed

--- app.js
+++ app.js
@@ -6,11 +6,20 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+var delim = '!'; //standard delimiter for commands. default is !, but can be changed.
+
 client.on('message', message => {
-	console.log(message.author + ': ' + message.content);
-	if (message.content === '!ping') {
+	console.log(message.author + ': ' + message.content); //logs every message
+	//!ping returns Pong
+	if (message.content === delim + 'ping') {
 		message.channel.send('Pong');
+	} 
+	//!parsha returns weekly parsha
+	else if (message.content === delim + 'parsha') {
+
 	}
+
+
 });
 
 client.login(process.env.TOKEN);
