74b4eb326ff95cb3785f13925c30750a82a2b0ea
aweizman <adamweizman@gmail.com>
2021-07-03 17:53:50 -0400

    removed JSON.stringify

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ client.on('message', async message => {
 
 		const { calendar_items } = await fetch('http://www.sefaria.org/api/calendars?timezone=America/New_York').then(res => res.json());
 
-		const { text } = await fetch('http://www.sefaria.org/api/text' + JSON.stringify(calendar_items[0].url)).then(res => res.json());
+		const { text } = await fetch('http://www.sefaria.org/api/text' + calendar_items[0].url).then(res => res.json());
 
 		const parsha = new MessageEmbed()
 		.setTitle(calendar_items[0].title.en + ' | ' + calendar_items[0].title.he)
