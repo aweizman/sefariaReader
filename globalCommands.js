31c5fa13fdeb68e82f8bf397f3c5ab81b9dec811
aweizman <adamweizman@gmail.com>
2021-08-30 15:39:57 -0400

    added global commands to launch when app is ready

---
+++ globalCommands.js
@@ -0,0 +1,33 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+require('dotenv').config();
+const fs = require('fs');
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+
+// temporarily used for testing on discord server
+const client_id = "854421123362193429";
+const guild_id = "854748615427751976";
+
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(
+			Routes.applicationCommands(client_id),
+			{ body: commands },
+		);
+
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
